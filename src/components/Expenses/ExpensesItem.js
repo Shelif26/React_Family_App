4977c81ee25ef127992eeef288843316950c5825
M-A-Shelif <shelifshelif30@gmail.com>
2022-12-29 17:08:35 +0530

    max_change

---
+++ src/components/Expenses/ExpensesItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./ExpensesItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+function ExpensesItems(props) {
+  // const [title, setTitle] = useState(props.title);
+  // const clickHandler = () => {
+  //   setTitle("updated");
+  //   console.log(title);
+
+  return (
+    <Card className="expense-item ">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpensesItems;
